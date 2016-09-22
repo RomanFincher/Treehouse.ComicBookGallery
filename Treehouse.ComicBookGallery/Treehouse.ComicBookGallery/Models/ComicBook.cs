4125499498ed42513c5cc694f0cb93665bd911c7
RomanFincher <romanxfincher@gmail.com>
2016-09-22 08:01:46 -0500

    Added Display text and CoverImageFileName properties to ComicBook model.

--- Treehouse.ComicBookGallery/Treehouse.ComicBookGallery/Models/ComicBook.cs
+++ Treehouse.ComicBookGallery/Treehouse.ComicBookGallery/Models/ComicBook.cs
@@ -7,11 +7,27 @@ namespace Treehouse.ComicBookGallery.Models
 {
     public class ComicBook
     {
-        public string Name { get; set; }
+        public string Id { get; set; }
         public string SeriesTitle { get; set; }
         public int IssueNumber { get; set; }
         public string DescriptionHtml { get; set; }
         public Artist[] Artists { get; set; }
         public bool Favorite { get; set; }
+
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + ": Issue #" + IssueNumber;
+            }
+        }
+
+        public string CoverImageFileName
+        {
+            get
+            {
+                return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg";
+            }
+        }
     }
 }
